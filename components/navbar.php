efdec3778ad9c23034dcf3882cecd7b7f073171c
Shamsddine <goumdissshamsddine@gmail.com>
2023-12-20 16:15:28 +0100

    debut

---
+++ components/navbar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header-container">
+        <header>
+            <h2>AFPA</h2>
+            <div class="navbar">
+                <a href="">Trouvez votre centre</a>
+                <a href="">Vos questions</a>
+                <div class="profil">
+                <i class="bi bi-person-fill"> Votre compte</i>
+                </div>
+            </div>
+        </header>
+    </div>
+</body>
+</html>
